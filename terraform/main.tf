0c7e11d0c94f8a05a0f33d1397fd8ce28286fded
Andrei Charniauski <andrei.charniauski@ds.pl>
2023-08-24 21:16:51 +0200

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,26 @@
+#resource "aws_s3_bucket" "website_redirect" {
+#  bucket = "676293462015"
+#}
+#
+#provider "aws" {
+#  profile = "default"
+#  region  = "eu-west-3"
+
+#  assume_role {
+#    role_arn = "arn:aws:iam::990472328692:role/role-np-sformproduct-stage-automation"
+#  }
+#}
+terraform {
+  backend "local" {}
+}
+
 resource "aws_s3_bucket" "website_redirect" {
   bucket = "676293462015"
 }
 
 provider "aws" {
-  profile = "default"
+  alias = "test"
   region  = "eu-west-3"
 
-  assume_role {
-    role_arn = "arn:aws:iam::990472328692:role/role-np-sformproduct-stage-automation"
-  }
-}
-terraform {
-  backend "local" {}
+  # ... (any additional provider configuration)
 }
